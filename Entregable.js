2a3f8fcac73f921105860bbd66df0faba33e14fa
GROOT-GEEK <ddurango712@soyudemedellin.edu.co>
2022-04-13 22:07:38 -0500

    -Entregable

---
+++ Entregable.js
@@ -0,0 +1,14 @@
+//Entregable 3 Desarrollo Web
+
+/*
+Hola profe buena noches adjunto repositiorio con la entrega
+
+https://github.com/GROOT-GEEK/api-airbnb/invitations
+
+*/
+
+function autor(){
+    let nombre = "Diego Alejandro Durango Garro"
+    console.log(nombre)
+}
+autor();
